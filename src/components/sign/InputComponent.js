d55fdf166a62d8926969d003e222b9dd90e81453
sh18542@naver.com <>
2020-06-17 20:22:46 +0900

    feature: password input 수정

--- src/components/sign/InputComponent.js
+++ src/components/sign/InputComponent.js
@@ -4,13 +4,21 @@ import './InputComponent.scss';
 const InputComponent = ({ name, placeholder }) => {
   return (
     <div className='inputContainer'>
-      {/* <div className={name + 'Image'}></div> */}
-      <input
-        className='input100'
-        type='text'
-        name={name}
-        placeholder={placeholder}
-      />
+      {name === 'password' ? (
+        <input
+          className='input100'
+          type='password'
+          name={name}
+          placeholder={placeholder}
+        />
+      ) : (
+        <input
+          className='input100'
+          type='text'
+          name={name}
+          placeholder={placeholder}
+        />
+      )}
       <span className='symbolInput'>
         <i className={name + 'Image'}></i>
       </span>
